b3328bba3e585fdf943460218816639318b08cb4
Rogerio <rogerioalves.ee@gmail.com>
2024-01-13 16:44:44 -0300

    fix mes decomp

--- app/adapters/reservoirrulerepository.py
+++ app/adapters/reservoirrulerepository.py
@@ -783,7 +783,7 @@ class NEWAVEReservoirRuleRepository(AbstractReservoirRuleRepository):
                     month=dadger.dt.mes,
                     day=dadger.dt.dia,
                 )
-                decompActualMonth = (decompDate + timedelta(days=6)).month
+                decompActualMonth = (decompDate + timedelta(days=8)).month
                 Log.log().info(f"Mês do DECOMP: {decompActualMonth}")
                 if decompActualMonth == previousMonth:
                     right_source_uow = s
@@ -1072,7 +1072,7 @@ class DECOMPReservoirRuleRepository(AbstractReservoirRuleRepository):
                 month=dadger.dt.mes,
                 day=dadger.dt.dia,
             )
-            decompActualDate = decompDate + timedelta(days=6)
+            decompActualDate = decompDate + timedelta(days=8)
             endDayMaps = {
                 1: datetime(
                     year=decompActualDate.year,
@@ -1182,7 +1182,7 @@ class DECOMPReservoirRuleRepository(AbstractReservoirRuleRepository):
             day=currentDadger.dt.dia,
         )
         currentDecompActualMonth = (
-            currentDecompDate + timedelta(days=6)
+            currentDecompDate + timedelta(days=8)
         ).month
         previousMonth = (
             12
@@ -1202,7 +1202,7 @@ class DECOMPReservoirRuleRepository(AbstractReservoirRuleRepository):
                     month=dadger.dt.mes,
                     day=dadger.dt.dia,
                 )
-                decompActualMonth = (decompDate + timedelta(days=6)).month
+                decompActualMonth = (decompDate + timedelta(days=8)).month
                 if decompActualMonth == previousMonth:
                     right_source_uow = s
 
